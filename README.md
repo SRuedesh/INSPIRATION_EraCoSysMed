7f9c22fd147352877c7d07c73a17be5db358d712
SRuedesh <sruedesh@gmail.com>
2021-07-13 16:31:12 +0200

    entered short description in readme

--- README.md
+++ README.md
@@ -6,3 +6,21 @@ Schema for the database, last updated 06/30/2021.
 
 
 <img src="./database_schema/database_schema_30_06_2021.svg">
+
+## Setting up the REST-API
+1. Install node and npm.
+2. In node_restapi/db create a folder 'config' and in it a 'config.json' file.
+3. The config.json should be formatted as follows:
+    ```json
+    {
+        "development": {
+        "host": "host_name",
+        "database": "db_name",
+        "user": "user_name",
+        "password": "user_password",
+        "port": port, default is 3306
+        }
+    }
+    ```
+4. Navigate to node_restapi and run "npm run dev"
+5. To test, enter "localhost:3000/api/compounds" in your browser.
--- node_restapi/server/server.js
+++ node_restapi/server/server.js
@@ -1,25 +1,10 @@
 const express = require("express");
 const apiRouter = require("./routes");
-// const exphbs = require("express-handlebars");
 const path = require("path");
 const db = require("./db");
 
 const app = express();
 
-// // Handlebars
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
-
-// // Routes
-// app.get("/", (req, res) => res.render("home"));
-// app.get("/documentation", (req, res) => res.render("documentation"));
-// app.get("/about", (req, res) => res.render("about"));
-// app.get("/contact", (req, res) => res.render("contact"));
-// app.get("/results", require("./routes"));
-
-// // Set static folder
-// app.use(express.static("./public"));
-// app.use("/css", express.static(__dirname + "public/css"));
 
 app.use(express.json());
 app.use("/api", apiRouter);
